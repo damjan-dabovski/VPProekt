26f58fb6d6994d9a0aea1d54ca3adec5ad72fe8f
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2018-06-17 14:15:08 +0200

    Added state framework

---
+++ SSE/MenuState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSE {
+    public class MenuState {
+        public Button[] buttons;
+        public int width, height;
+        public Image background;
+
+        public MenuState(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void draw(Graphics g) {
+            g.DrawImage(this.background, new Point(0, 0));
+        }
+    }
+}
---
+++ SSE/TutorialScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSE {
+    public class TutorialScreen : MenuState {
+        public Button back;
+
+        public TutorialScreen(int width, int height): base(width,height){
+            back = new Button();
+            this.buttons = new Button[1] { back };
+            //TODO custom image and button positioning
+        }
+    }
+}
